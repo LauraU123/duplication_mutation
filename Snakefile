29d64efe6eec8774fb0d8da423fa03149e0a70a6
LauraU123 <laura.urbanska@stud.unibas.ch>
2023-04-25 14:58:07 +0200

    making progress to fix A and B

--- Snakefile
+++ Snakefile
@@ -1,11 +1,12 @@
 #this workflow reconstructs the mutations in the RSV A and RSV B duplicated regions of the G gene
 #graphs = expand("{a_or_b}/cumulative_sum_synonymous.png", "{a_or_b}/cumulative_sum_nonsynonymous.png", a_or_b=A_OR_B)
 configfile: "config/configfile.yaml"
-A_OR_B = ["a", "b"]
+A_OR_B = ["a"]
+
 
 rule all:
     input:
-        expand("results/{a_or_b}/only_duplication.fasta", a_or_b=A_OR_B)
+        expand("results/{a_or_b}/reconstructed.fasta", a_or_b=A_OR_B)
 
 rule branch_from_root:
     input:
@@ -40,6 +41,7 @@ rule align_to_ref:
           {input.reconstructed_seq}
         """
 
+
 rule just_duplication:
     input:
         reconstructed_seq = rules.align_to_ref.output.aligned
@@ -59,13 +61,34 @@ rule just_duplication:
 
 rule align_G:
     input:
-        only_dupl = rules.just_duplication.output.only_dupl
+        only_dupl = rules.just_duplication.output.only_dupl,
+        reference = rules.align_to_ref.input.reference
     output:
         aligned_dupl = "results/{a_or_b}/aligned_duplication.fasta"
     shell:
         """
         augur align --nthreads 4 \
         --sequences {input.only_dupl} \
-        --reference-sequence {input.reference} \
         --output {output.aligned_dupl}
         """
+
+rule reconstruct:
+    input:
+        aligned_seq = rules.align_G.output.aligned_dupl,
+        tree = rules.branch_from_root.input.tree,
+    output:
+        reconstructed_fasta = "results/{a_or_b}/reconstructed.fasta"
+    params:
+        len_ = lambda w: config["reconstruct"].get(w.a_or_b),
+        intermediate = "results/{a_or_b}/intermediate.fasta"
+    shell:
+        """
+        python3 scripts/reconstruct_the_nodes.py \
+        --length {params.len_} \
+        --intermediate {params.intermediate} \
+        --output {output.reconstructed_fasta} \
+        --input-tree {input.tree} \
+        --input-alignment {input.aligned_seq}
+        """
+
+rule graph:
--- scripts/reconstruct_the_nodes.py
+++ scripts/reconstruct_the_nodes.py
@@ -3,7 +3,6 @@ from Bio import SeqIO, SeqRecord, Seq, Phylo
 from collections import Counter, defaultdict
 import pandas as pd
 
-
 def reconstruct_insertions_(fasta, tree, output):
     tree_ = Phylo.read(tree, "newick")
     f = SeqIO.parse(fasta, "fasta")
@@ -64,6 +63,7 @@ def reconstruct_insertions_(fasta, tree, output):
     for i, j in seq_dict.items():
         entry = SeqRecord.SeqRecord(Seq.Seq(j), id=i)
         new_file.append(entry)
+    print(new_file)
     SeqIO.write(new_file, output, "fasta")
     return(mut_dict)
 
@@ -94,20 +94,21 @@ if __name__=="__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
     parser.add_argument('--input-alignment', required=True, type=str, help="input root sequence")
+    parser.add_argument('--intermediate', type=str,  help="length of duplication file")
     parser.add_argument('--length', type=str,  help="length of duplication file")
     parser.add_argument('--input-tree', type=str,  help="input newick tree")
     parser.add_argument('--output', type=str, help="output fasta")
     args = parser.parse_args()
 
 
-    unprocessed_mutations = reconstruct_insertions_(args.input_alignment, args.input_tree, "results/{a_or_b}/intermediate_file.fasta")
+    unprocessed_mutations = reconstruct_insertions_(args.input_alignment, args.input_tree, args.intermediate)
+    old_file = SeqIO.parse(args.intermediate, "fasta")
     tree_ = Phylo.read(args.input_tree, "newick")
     tree_.root_at_midpoint()
     tree_.find_clades()
-
     mutations_for_each_node_same = defaultdict(list)
 
-    for i in range(0, 144):
+    for i in range(0, int(args.length)):
         dictionary_for_loc = dict()
         for node, entry in unprocessed_mutations.items():
             for e in entry:
@@ -147,62 +148,22 @@ if __name__=="__main__":
                             dictionary_for_loc[branch.name] = parent_seq.replace('X', '')
                 
         last_one = dict()
+        same_ = dict()
         for node, j in dictionary_for_loc.items():
+            if len(Counter(dictionary_for_loc[node])) != 1:
+                last_one[node] = j
             if len(Counter(dictionary_for_loc[node])) == 1:
                 mutations_for_each_node_same[node].append(f'{i}:{j}')
-            else: last_one[node] = j
 
-    old_file = SeqIO.parse("results/{a_or_b}/intermediate_file.fasta", "fasta")
-    update_file_1 = []
-
+    update_file = []
     for entry in old_file:
         if entry.id in mutations_for_each_node_same:
             for mut_ in mutations_for_each_node_same[entry.id]:
                 mut_loc = int(find_digit(mut_)[0])
                 if (entry.seq[mut_loc]) == 'X':
                     entry.seq = entry.seq[:mut_loc] + mut_[-1] + entry.seq[mut_loc+1:]
-            new_entry = SeqRecord(Seq(entry.seq), id=entry.id, description=entry.description)
-            update_file_1.append(new_entry)
+            new_entry = SeqRecord.SeqRecord(Seq.Seq(entry.seq), id=entry.id, description=entry.description)
+            update_file.append(new_entry)
         else: 
-            update_file_1.append(entry)
-
-    seq_dict = dict()
-    update_file = []
-    for record in update_file_1:
-        seq_dict[record.id] = record.seq
-        if "X" not in record.seq:
-            update_file.append(record)
-    for branch in tree_.get_nonterminals(order='postorder'):
-        all_branch_seq = []
-        if pd.isna(branch.name) == False:
-            if branch.name in seq_dict:
-                if 'X' in seq_dict[branch.name]:
-                    indices_of_interest = (find(seq_dict[branch.name], 'X'))
-                    branch_names = []
-                    for b in branch:
-                        if b.is_terminal() == False:
-                            branch_names.append(b)
-                    if len(branch_names) == 1:
-                        for ind in indices_of_interest:
-                            if (seq_dict[branch_names[0].name][ind]) != 'X':
-                                seq_dict[branch.name] = seq_dict[branch.name][:ind] + seq_dict[branch_names[0].name][ind]+ seq_dict[branch.name][ind+1:]
-                            else:
-                                which_one = [seq_dict[branch_names[0].get_terminals()[0].name][ind], seq_dict[branch_names[0].get_terminals()[1].name][ind]]
-                                for b in branch:
-                                    if b.is_terminal() == True:
-                                        which_one.append(seq_dict[b.name][ind])
-                                new_letter = (max(Counter(which_one).keys()))
-                                seq_dict[branch.name] = seq_dict[branch.name][:ind] + new_letter + seq_dict[branch.name][ind+1:]
-
-                        new_entry = SeqRecord(Seq(seq_dict[branch.name]), id=branch.name, description=branch.name)
-
-                    all_term = dict()
-                    if len(branch_names) > 1:
-                        for i in range(0, len(branch_names)):
-                            all_term[branch_names[i].name]= len(branch_names[i].get_terminals())
-                        for ind in indices_of_interest:
-                            seq_dict[branch.name] = seq_dict[branch.name][:ind] + seq_dict[max(all_term)][ind]+ seq_dict[branch.name][ind+1:]
-                        new_entry = SeqRecord(Seq(seq_dict[branch.name]), id=branch.name, description=branch.name)
-                    update_file.append(new_entry)
-
-    SeqIO.write(update_file, args.output, 'fasta')
+            update_file.append(entry)
+    SeqIO.write(update_file, args.output, "fasta")
